351db829840600c99e727a356e7f57073532fa8d
Xorlent <94985297+Xorlent@users.noreply.github.com>
2023-10-24 16:30:01 -0700

    Update savehostkey.cmd

--- savehostkey.cmd
+++ savehostkey.cmd
@@ -1,1 +1,3 @@
+REM No need to change any parameters here.
+REM The command below is not used for anything more than to tell plink.exe to store the SSH key.
 echo y | C:\Scripts\Cisco\plink.exe -ssh swbackup@%1 "exit"
