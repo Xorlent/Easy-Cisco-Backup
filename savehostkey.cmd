f057eb9b168cd22384429122ca7854ee3c45ffff
Xorlent <94985297+Xorlent@users.noreply.github.com>
2023-10-27 11:33:57 -0700

    Moving all configurable parameters to XML file

--- savehostkey.cmd
+++ savehostkey.cmd
@@ -1,3 +1,3 @@
 REM No need to change any parameters here.  The username is a placeholder only.
 REM The command below is not used for anything more than to tell plink.exe to store the SSH key.
-echo y | C:\Scripts\Cisco\plink.exe -ssh swbackup@%1 "exit"
+echo y | %1 -ssh ciscobackup@%2 "exit"
